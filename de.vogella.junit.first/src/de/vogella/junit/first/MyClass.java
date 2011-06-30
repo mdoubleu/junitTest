746ad91a37c283e47b330d598fba218376714133
Michael Wineburg <mikedoubleyouu@Michael-Wineburgs-MacBook-Pro.local>
2011-06-29 20:47:04 -0400

    junit

---
+++ de.vogella.junit.first/src/de/vogella/junit/first/MyClass.java
@@ -0,0 +1,13 @@
+package de.vogella.junit.first;
+
+public class MyClass {
+	public int multiply(int x, int y) {
+		return x * y;
+	}
+public int addition(int x, int y){
+		
+		return x+y;
+		
+	}
+
+}
